6b1606e453f47808b791fb8fe90716ba1a693d95
Aaron Torres <yungsofa@yahoo.com>
2024-03-03 04:43:02 -0800

    Added Enemy Animation Blueprint Added Goblin Slingshot enemy Added Goblin Spear and Goblin Slingshot Animation blueprints Added InputMappingContext Added Input Actions "WASD" Added PlayerController Class which is AuraPlayerController

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,14 @@
+// Tutorial GAS
+
+
+#include "Player/AuraPlayerController.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+		Super::BeginPlay();
+}
